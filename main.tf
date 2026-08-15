7148b1f1ffa251bd80bc9c98427e5cb095d990eb
Sabspratik22 <sabalkarpratik137@gmail.com>
2026-08-15 22:11:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -91,7 +91,7 @@ resource "aws_instance" "server" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = var.instance_type
   subnet_id              = aws_subnet.public.id
-  key_name               = "key-id"
+  key_name               = "abc"
 
   vpc_security_group_ids = [
     aws_security_group.web_sg.id
