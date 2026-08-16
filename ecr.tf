9d3ff518a59bc947846c63a9102deb35f0b92ce7
Sabspratik22 <sabalkarpratik137@gmail.com>
2026-08-16 11:12:47 +0530

    Update ecr.tf

--- ecr.tf
+++ ecr.tf
@@ -1,1 +1,38 @@
+resource "aws_ecr_repository" "app_repo" {
+  name                 = "my-app-repo"
+  image_tag_mutability = "MUTABLE"
 
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = {
+    Environment = "dev"
+    ManagedBy   = "terraform"
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "app_repo_policy" {
+  repository = aws_ecr_repository.app_repo.name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "Keep only last 10 images"
+        selection = {
+          tagStatus   = "any"
+          countType   = "imageCountMoreThan"
+          countNumber = 10
+        }
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+}
+
+output "ecr_repository_url" {
+  value = aws_ecr_repository.app_repo.repository_url
+}
