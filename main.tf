eef378af8ead5c7eb494d86ae3584b7cbeba75c2
Sabspratik22 <sabalkarpratik137@gmail.com>
2026-08-21 23:39:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -95,39 +95,3 @@ resource "aws_instance" "server" {
     volume_size = 20
   }
 }
-
-resource "aws_ecr_repository" "app_repos" {
-  name                 = "my-app-repos"
-  image_tag_mutability = "MUTABLE"
-  force_delete         = true   # allows terraform destroy even if images exist in the repo
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-
-  tags = {
-    Environment = "dev"
-    ManagedBy   = "terraform"
-  }
-}
-
-resource "aws_ecr_lifecycle_policy" "app_repo_policy" {
-  repository = aws_ecr_repository.app_repo.name
-
-  policy = jsonencode({
-    rules = [
-      {
-        rulePriority = 1
-        description  = "Keep only last 10 images"
-        selection = {
-          tagStatus   = "any"
-          countType   = "imageCountMoreThan"
-          countNumber = 10
-        }
-        action = {
-          type = "expire"
-        }
-      }
-    ]
-  })
-}
