07a4673e7b1a4e45d1e8d84d6db28323f50b4237
Sabspratik22 <sabalkarpratik137@gmail.com>
2026-08-22 10:35:51 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,16 +1,1 @@
-terraform {
-  backend "s3" {
-    bucket         = "my-project-terraform-state-bucket"
-    key            = "jenkins-infra/terraform.tfstate"
-    region         = "eu-central-1"
-    dynamodb_table = "terraform-state-lock"
-    encrypt        = true
-  }
 
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
